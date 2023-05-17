f1d8cd6a5384fdcbfadc4de4739ddffb37483162
xi_jjun <rlawowns97@naver.com>
2023-05-17 23:20:09 +0900

    kimjaejun(230518) : 문자열 키를 symbol로 수정

--- chapter1/study_2023_0518/20230518_kimjaejun_refactoring/main.rb
+++ chapter1/study_2023_0518/20230518_kimjaejun_refactoring/main.rb
@@ -3,10 +3,10 @@ require 'json'
 def statement(invoice, plays)
   total_amount = 0
   volume_credits = 0
-  result = "청구 내역 (고객명: #{invoice["customer"]})\n"
+  result = "청구 내역 (고객명: #{invoice[:customer]})\n"
 
-  invoice["performances"].each do |perf|
-    play = plays[perf["playID"]]
+  invoice[:performances].each do |perf|
+    play = plays[perf[:playID].to_sym]
 
     # 금액 계산
     this_amount = calculate_amount(perf, play)
@@ -15,7 +15,7 @@ def statement(invoice, plays)
     volume_credits += accumulate_credit(perf, play, volume_credits)
 
     # 청구 내역을 출력한다.
-    result += "  #{play["name"]}: $#{'%.2f' % (this_amount / 100)} (#{perf["audience"]}석)\n"
+    result += "  #{play[:name]}: $#{'%.2f' % (this_amount / 100)} (#{perf[:audience]}석)\n"
     total_amount += this_amount
   end
 
@@ -26,9 +26,9 @@ def statement(invoice, plays)
 end
 
 def accumulate_credit(perf, play, volume_credits)
-  volume_credits += [perf["audience"] - 30, 0].max
+  volume_credits += [perf[:audience] - 30, 0].max
   # 희극 관객 5명마다 추가 포인트를 제공한다.
-  volume_credits += (perf["audience"] / 5).floor(2) if "comedy" == play["type"]
+  volume_credits += (perf[:audience] / 5).floor(2) if "comedy" == play[:type]
 
   volume_credits
 end
@@ -36,20 +36,20 @@ end
 def calculate_amount(perf, play)
   this_amount = 0
 
-  case play["type"]
+  case play[:type]
   when "tragedy"
     this_amount = 40000
-    if perf["audience"] > 30
-      this_amount += 1000 * (perf["audience"] - 30)
+    if perf[:audience] > 30
+      this_amount += 1000 * (perf[:audience] - 30)
     end
   when "comedy"
     this_amount = 30000
-    if perf["audience"] > 20
-      this_amount += 10000 + 500 * (perf["audience"] - 20)
+    if perf[:audience] > 20
+      this_amount += 10000 + 500 * (perf[:audience] - 20)
     end
-    this_amount += 300 * perf["audience"]
+    this_amount += 300 * perf[:audience]
   else
-    raise "알 수 없는 장르: #{play["type"]}"
+    raise "알 수 없는 장르: #{play[:type]}"
   end
 
   this_amount
@@ -57,8 +57,8 @@ end
 
 
 # data
-invoices = JSON.parse(File.read('./invoices.json'))
-plays = JSON.parse(File.read('./plays.json'))
+invoices = JSON.parse(File.read('./invoices.json'), symbolize_names: true)
+plays = JSON.parse(File.read('./plays.json'), symbolize_names: true)
 
 invoices.each do |invoice|
   puts statement invoice, plays
